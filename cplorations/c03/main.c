e2105200aa83190e47bcb82e9163f60d6ae9a7aa
Seth Rothstein <sethmrothstein@gmail.com>
2023-10-19 10:44:22 -0700

    Finished c03

---
+++ cplorations/c03/main.c
@@ -0,0 +1,55 @@
+/****************************************
+ * C-ploration 3 for CS 271
+ * 
+ * [NAME] Seth Rothstein
+ * [TERM] FALL 2023
+ * 
+ ****************************************/
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+#define MAX_LINE_LENGTH  200
+
+int main(int argc, const char *argv[])
+{	
+	if (argc != 2) {
+		// if incorrect number of arguments
+		printf("Usage: %s [filename]\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+
+	FILE *fin = fopen( argv[1], "r" );
+
+	if (fin == NULL) {
+		perror("Unable to open file!");
+		exit(EXIT_FAILURE);
+	}
+
+	char out_file[MAX_LINE_LENGTH];
+	strcpy(out_file, argv[1]);
+	strcat(out_file, ".echo");
+
+	FILE *fout = fopen( out_file, "w+" );
+
+	if (fout == NULL) {
+		perror("Unable to open file!");
+		exit(EXIT_FAILURE);
+	}
+
+	char line[MAX_LINE_LENGTH] = {0};
+	unsigned int line_num = 0;
+
+	while (fgets(line, sizeof(line), fin)) {
+		line_num++;
+		printf("[%04d] %s", line_num, line);
+		fprintf(fout, line);
+	}
+
+	fclose(fin);
+	fclose(fout);
+
+
+	return 1;
+}
