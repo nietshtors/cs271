96445adc4a97fc6fe9b0991906f06e43c028a9ab
Seth Rothstein <sethmrothstein@gmail.com>
2023-11-04 07:50:58 -0600

    Finished Mult

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -9,4 +9,39 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+@ARG
+M=0                 // Zero output
+@THIS
+M=0                 // Zero run count
+@SP
+D=M                 // Load R0
+@SKIP
+D;JEQ               // Skips loop if R0 is 0
+@LCL
+D=M                 // Load R1
+@SKIP
+D;JEQ               // Skips loop if R1 is 0
+
+(LOOP)
+@ARG
+D=M                 // Load total
+@SP
+D=D+M               // Add next to running total
+@ARG
+M=D                 // Save running total
+@THIS
+MD=M+1              // Increase run count
+@LCL
+D=M-D               // Subtract run count from R1
+@LOOP
+D;JGT
+@INFINITE_LOOP
+0;JMP
+
+(SKIP)
+@ARG
+M=0
+
+(INFINITE_LOOP)
+@INFINITE_LOOP
+0;JMP               // Infinite loop
