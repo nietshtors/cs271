ca649116752c434089ce34f9f98ca5d8d529b967
Seth Rothstein <sethmrothstein@gmail.com>
2023-11-07 10:38:28 -0800

    Finished Fill

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -11,4 +11,43 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+(KEYBOARD)
+@SCREEN
+D=A                 // Load screen address to D
+@R0
+M=D                 // Save initial screen address to R0
+@KBD
+D=M                 // Load keyboard input to D
+@BLACK
+D;JGT               // If keyboard, jump to black
+@WHITE
+D;JEQ               // Else, jump to white
+@KEYBOARD
+0;JMP               // Backup catch
+
+(WHITE)
+@R1
+M=0                 // Save "white" to R1
+@FILL_SCREEN
+0;JMP
+
+(BLACK)
+@R1 
+M=-1                // Save "black" to R1
+@FILL_SCREEN
+0;JMP
+
+(FILL_SCREEN)
+@R1
+D=M                 // Load color to D
+@R0
+A=M                 // Set address to screen point
+M=D                 // Save color to row of pixels
+@R0
+MD=M+1              // Increment address to next set and save to D
+@KBD
+D=A-D               // Subtract KBD address from current point on screen
+@FILL_SCREEN
+D;JGT               // If difference is > 0, loop
+@KEYBOARD
+D;JLE               // Else, jump to keyboard loop
